cc970eb81968fb9afa90175ddfeb20f94eb43a77
Bill Beers <nothingatall544@gmail.com>
2013-04-02 19:02:17 -0400

    added a ListView and dummmy data to the gameRoomsView

--- CardsAgainstAgility/src/com/adaba/MainActivity.java
+++ CardsAgainstAgility/src/com/adaba/MainActivity.java
@@ -1,8 +1,13 @@
 package com.adaba;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +15,20 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		List<String> listOfGames = new LinkedList<String>();
+		listOfGames.add("first");
+		listOfGames.add("second");
+		listOfGames.add("third");
+		listOfGames.add("forth");
+		listOfGames.add("fifth");
+		listOfGames.add("sixth");
+		listOfGames.add("seventh");
+		
+		ListView gameList = (ListView) findViewById(R.id.gameRoomList);
+		ArrayAdapter<String> adapt = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, listOfGames);
+		gameList.setAdapter(adapt);
+		
 	}
 
 	@Override
