a4f488cdf42dc9902e69c37f7721a391374fe001
adl2158 <adl2158@rit.edu>
2013-04-17 11:37:28 -0700

    Lobby and selection activities

---
+++ CardsAgainstAgility/src/com/adaba/activities/LobbyViewActivity.java
@@ -0,0 +1,15 @@
+package com.adaba.activities;
+
+import com.adaba.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LobbyViewActivity extends Activity {
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_view_lobby);
+
+	}
+}
